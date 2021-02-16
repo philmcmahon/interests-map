5df055da6dfe21d7eeb9e58dbda113d7d35918f6
Philip McMahon <philip.mcmahon@guardian.co.uk>
2021-02-16 19:47:34 +0000

    Add title, description

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -8,9 +8,14 @@ import Peace from "../assets/peace.png";
 import { fetchAllLocations } from "./../services/database";
 import console = require("console");
 
-const FullSizeDiv = styled.div`
-  width: 100%;
-  height: 100%;
+const MapDiv = styled.div`
+  width: 80%;
+  padding-left: 20%;
+`;
+
+const TitleDiv = styled.div`
+  width: 90%;
+  padding-left: 10%;
 `;
 
 export const Museo500Div = styled.div`
@@ -24,13 +29,16 @@ const generateIcon = (iconUrl: string, shadowUrl?: string) => {
     iconUrl,
     iconSize: [20, 20],
     iconAnchor: [10, 10],
-    popupAnchor: [0, -10]
+    popupAnchor: [0, -10],
     // shadowSize: [68, 95],
     // shadowAnchor: [22, 94]
   });
 };
 
-const startPosition: Leaflet.LatLngExpression = [53.80618473785163, -1.5504965349684796];
+const startPosition: Leaflet.LatLngExpression = [
+  53.80618473785163,
+  -1.5504965349684796,
+];
 
 export interface PointOfInterest {
   title: string;
@@ -52,49 +60,52 @@ export interface AirTableRecord {
 
 class ColonialismMap extends React.Component {
   state = {
-    pointsOfInterest: new Array<PointOfInterest>()
+    pointsOfInterest: new Array<PointOfInterest>(),
   };
 
   componentDidMount = () => {
-    fetchAllLocations().then(pointsOfInterest => {
-      console.log(pointsOfInterest)
+    fetchAllLocations().then((pointsOfInterest) => {
+      console.log(pointsOfInterest);
       this.setState({ pointsOfInterest });
     });
   };
 
   makeMarkers = (pois: PointOfInterest[]): any => {
-    return pois.map(poi => {
+    return pois.map((poi) => {
       const position: Leaflet.LatLngExpression = [
         Number(poi.latitude),
-        Number(poi.longitude)
+        Number(poi.longitude),
       ];
 
-      const markerIcon = Bomb
+      const markerIcon = Bomb;
 
       return (
-        <Marker position={position} key={poi.id} icon={generateIcon(markerIcon)}>
+        <Marker
+          position={position}
+          key={poi.id}
+          icon={generateIcon(markerIcon)}
+        >
           <Popup>
             <Museo500Div className="card blue-grey darken-1">
               <div className="card-image">
-                
-                  <img
-                    src={poi.image || `https://logo.clearbit.com/${encodeURI(poi.title)}`}
-                    alt={poi.title}
-                    style={{ width: "100%" }}
-                  />
-                
+                <img
+                  src={poi.image}
+                  alt={poi.title}
+                  style={{ width: "100%" }}
+                />
               </div>
               <Museo500Div className="card-content white-text">
                 <span className="card-title">{poi.title}</span>
-                {poi.riscLinks && (<>
-                  <b>Links to institutions in RiSC network</b>
-                  <p>{poi.riscLinks}</p>
-                </>)} 
+                {poi.riscLinks && (
+                  <>
+                    <b>Links to institutions in RiSC network</b>
+                    <p>{poi.riscLinks}</p>
+                  </>
+                )}
               </Museo500Div>
               <Museo500Div className="card-action">
-                {poi.source && (<a href={poi.source} >Read More</a>)}
+                {poi.source && <a href={poi.source}>Read More</a>}
               </Museo500Div>
-              
             </Museo500Div>
           </Popup>
         </Marker>
@@ -104,21 +115,34 @@ class ColonialismMap extends React.Component {
 
   render() {
     return (
-      <FullSizeDiv>
-        <MapContainer
-          center={startPosition}
-          zoom={6}
-          preferCanvas={true}
-          style={{ height: "1000px" }}
-        >
-          <TileLayer
-            id="mapbox/streets-v11"
-            url={`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoicG1jbWFob24xIiwiYSI6ImNqd294ZW4xMDBiMW80YnFyYzhheGo2NXMifQ.OAueHLCYkqOg4qbND3CvHg`} // TODO: mapbox access token
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          />
-          {this.makeMarkers(this.state.pointsOfInterest)}
-        </MapContainer>
-      </FullSizeDiv>
+      <>
+        <TitleDiv>
+          <h1>UK Universities with links to Academic RiSC</h1>
+          <p>
+            You can use the map below to explore universities with links to the
+            academic resilience and security community network. In all cases you
+            can read our source data by clicking the 'read more' button. If
+            you're currently studying at one of these institutions and are
+            interested in campaigning for change head to the{" "}
+            <a href="https://caatunis.net/">CAAT Universities website</a>.
+          </p>
+        </TitleDiv>
+        <MapDiv>
+          <MapContainer
+            center={startPosition}
+            zoom={6}
+            preferCanvas={true}
+            style={{ height: "800px" }}
+          >
+            <TileLayer
+              id="mapbox/streets-v11"
+              url={`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoicG1jbWFob24xIiwiYSI6ImNqd294ZW4xMDBiMW80YnFyYzhheGo2NXMifQ.OAueHLCYkqOg4qbND3CvHg`} // TODO: mapbox access token
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            />
+            {this.makeMarkers(this.state.pointsOfInterest)}
+          </MapContainer>
+        </MapDiv>
+      </>
     );
   }
 }
